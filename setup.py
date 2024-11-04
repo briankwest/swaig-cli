cb304a454261538fdd590931b6b420d80d315d6c
Brian West <brian@signalwire.com>
2024-11-04 18:13:11 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="swaig-cli",
-    version="0.1.4",
+    version="0.1.5",
     author="Brian West",
     author_email="brian@signalwire.com",
     description="A command-line tool for testing SignalWire AI Gateway functions",
